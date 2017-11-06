b9860106c8455f926cde43ccfc01992c86585f05
mxito3 <1477311729@qq.com>
2017-11-06 21:34:51 +0800

    add nav_bar and head of website
    add nav_bar and head of website

---
+++ uestc-main/main.js
@@ -0,0 +1,40 @@
+/*
+* @Author: YP
+* @Date:   2017-11-06 12:42:16
+* @Last Modified by:   YP
+* @Last Modified time: 2017-11-06 20:35:53
+*/
+
+$(document).ready(function()
+{
+$(".commenWrapper").mouseover(function(e){
+var hoverDomObj = e.target.id;
+var className0=e.target.className;
+if(className0!='middle-nav-link')
+{
+	return;
+}
+var aim=$("#"+hoverDomObj).siblings();
+var result;
+for(var i=0;i<aim.length;i++)
+{
+	if(aim[i].tagName=='DIV')
+	{
+		result=aim[i].id;
+		
+	
+	    $("#"+result).slideDown("fast");
+	    break;
+        
+		
+	}
+
+}
+
+
+});
+
+$(".commenWrapper").mouseleave(function(){
+	$(".nav-dropdown-commen").slideUp("fast");
+});
+});
